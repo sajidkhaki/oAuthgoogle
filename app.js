ce8e274b125c7a15fae0b6d946794b738ebf8f01
sajidkhak <sajidkhaki@gmail.com>
2020-03-30 14:58:15 +0530

    added config file for google authenticator

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+
+// set up session cookies
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ config/keys.js
@@ -0,0 +1,15 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '616581710231-3gh4r5mnbeadi0r9h1gbvk844kbir61o.apps.googleusercontent.com',
+            clientSecret: 'y3kKg0rZlk3WhE5u-A5aHwsD'
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://sajidkhaki:sajidkhaki@cluster0-m9bee.mongodb.net/test?retryWrites=true&w=majority'
+
+        },
+        session: {
+            cookieKey: 'sajidashrafkhaki.firebaseapp.com/'
+        }
+};
