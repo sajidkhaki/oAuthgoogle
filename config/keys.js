d784223a143c7be57a106232c0ee79de3152fb04
sajidkhak <sajidkhaki@gmail.com>
2020-03-30 15:19:32 +0530

    secret key added

--- config/keys.js
+++ config/keys.js
@@ -2,11 +2,11 @@
 
 module.exports = {
         google: {
-            clientID: '616581710231-3gh4r5mnbeadi0r9h1gbvk844kbir61o.apps.googleusercontent.com',
-            clientSecret: 'y3kKg0rZlk3WhE5u-A5aHwsD'
+            clientID: '488618312999-qoonujoecve4vvuitni7c0ii8516nk3j.apps.googleusercontent.com',
+            clientSecret: 'kvgj6VlXsd3gaSL3ddUE5Je_'
         },
         mongodb: {
-            dbURI: 'mongodb+srv://sajidkhaki:sajidkhaki@cluster0-m9bee.mongodb.net/test?retryWrites=true&w=majority'
+            dbURI: 'mongodb+srv://sajidkhaki:sajid123@cluster0-m9bee.mongodb.net/test?retryWrites=true&w=majority'
 
         },
         session: {
